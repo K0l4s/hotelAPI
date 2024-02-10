b375edd09f05026c837844df7430b1d464fc911a
Trung Kien <21110223@student.hcmute.edu.vn>
2024-02-11 00:55:33 +0700

    contact

--- src/main/java/com/hotel/hotelapi/controller/CustomerController.java
+++ src/main/java/com/hotel/hotelapi/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.hotel.hotelapi.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
